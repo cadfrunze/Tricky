6b4b5deef47c83bad44f8cec2e8d9517c0b86a8f
cadfrunze <marius1890@yahoo.com>
2023-01-20 10:25:28 +0200

    new

---
+++ test4.py
@@ -0,0 +1,20 @@
+
+list1 = [1, 2, 3, 5, 4]
+
+max = 0
+
+for num in list1:
+    if num > max:
+        max = num
+print(max)
+
+list2 = ["1", "2", "3", "5", "4"]
+
+a = ""
+a = a.join(list2)
+print(a)
+b = "sdadwasdsd"
+c = b.split()
+print(c)
+
+
